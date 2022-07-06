6a665fd86bd5a8fc39cf31731be1fc7e83bd8672
Joseph Mathias <75617133+joseph-mathias@users.noreply.github.com>
2022-07-06 07:36:01 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	int i;
-	for ( i = 97; i <= 122 ; i++)	
-	{
+	for (i = 97;i <= 122; i++)	
+
 		if (!(i == 113 || i == 101))
 			putchar(i);
-	}
+
 	putchar('\n');
 	return (0);
 }
