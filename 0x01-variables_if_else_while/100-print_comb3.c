e40e2a275c070e722cc7656ebff44bb44fb58532
Joseph Mathias <75617133+joseph-mathias@users.noreply.github.com>
2022-07-06 08:09:55 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point
- * 
  * Return: Exit status is 0 (if success)
  */
 int main(void)
@@ -10,13 +9,13 @@ int main(void)
 	int i;
 	int j;
 
-	for(i = 48; i < 57; i++)
+	for (i = 48; i < 57; i++)
 	{
-		for(j = i + 1; j <= 57; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if(j == 57 && i == 56)
+			if (j == 57 && i == 56)
 				break;
 			putchar(44);
 			putchar(32);
