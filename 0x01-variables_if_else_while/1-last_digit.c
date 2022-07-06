660bde4b6110db24a742cc59b625bc05b185b0d6
Joseph Mathias <75617133+joseph-mathias@users.noreply.github.com>
2022-07-06 07:08:04 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Prints to the standard error
@@ -8,7 +9,7 @@
  */
 int main(void)
 {
-	int n;
+	int n, ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
