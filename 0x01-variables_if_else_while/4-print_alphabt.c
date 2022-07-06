038e8c2755be3a15c989d7120697d31079cd209e
Joseph Mathias <75617133+joseph-mathias@users.noreply.github.com>
2022-07-06 07:25:09 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,15 +4,17 @@
  *
  * main - Selectively prints alphabets to the screen
  * Return: Exit status is zero means success
- **/
+ */
 int main(void)
 {
 	int i;
+	
 	for (i = 97; i <= 122 ; i++)
 	{
 		if (!(i == 133 || i == 101))
 			putchar(i);
 	}
+	
 	putchar('\n');
 	return(0);
 }
